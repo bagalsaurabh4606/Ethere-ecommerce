5554054c3355650684b1a20e273e2e948b389053
saurabh bagal <bagalsaurabh644@gmail.com>
2024-10-11 17:12:25 +0530

    payment successfull page built

--- 3-Ethere-react/src/pages/PaymentSuccessful.jsx
+++ 3-Ethere-react/src/pages/PaymentSuccessful.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 
-const PaymentSuccessful =()=>
-{
-  return<>
-  <p>Payment successful</p>
-  <Link to={"/profile"}><button type="button" class="btn btn-info">Info</button></Link>
-  </> 
 
-}
+const PaymentSuccessful = () => {
+  return (
+    <div className="payment-success-wrapper">
+      <div className="payment-circle">
+        <div className="payment-checkmark">✔️</div>
+      </div>
+      <h1 className="payment-success-heading">Order Confirmed</h1>
+      <p className="payment-success-message">Thank you for your purchase! Your payment has been processed successfully.</p>
+      <Link to="/profile">
+        <button type="button" className="payment-profile-btn">Track Order</button>
+      </Link>
+    </div>
+  );
+};
 
 export default PaymentSuccessful;
