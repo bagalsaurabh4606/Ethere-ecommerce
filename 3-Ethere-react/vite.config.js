a3912c511c1d3f8d3107ac6e44fb06d9ddeee847
saurabh bagal <bagalsaurabh644@gmail.com>
2025-08-14 12:11:51 +0530

    minor change

--- 3-Ethere-react/vite.config.js
+++ 3-Ethere-react/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Ethere-ecommerce",
+  base: "/Ethere-ecommerce/",
   plugins: [react()],
 })
